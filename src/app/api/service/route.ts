159bb47567afb20fd8560cf95054ab552e596d75
shivamdev24 <shivam.dev2402@gmail.com>
2024-10-03 15:52:40 +0530

    update

--- src/app/api/service/route.ts
+++ src/app/api/service/route.ts
@@ -12,7 +12,7 @@ const authenticateRequest = async (request: NextRequest) => {
     throw new Error("No token provided");
   }
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY!);
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET!);
     return decoded;
   } catch (error) {
     throw new Error("Invalid token", error: error.message);
